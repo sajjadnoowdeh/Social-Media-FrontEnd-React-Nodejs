7bcb1ba6d13160f0bed765047fe8115fee33ebb1
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-06 23:44:14 +0430

    add compentent post to  feed

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Share from '../Share/Share';
-import Post from '../Post/Post';
+import Post from '../PostFeed/PostFeed';
 import "./Feed.style.css"
 export default function Feed() {
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share/>
-                {/* <Post /> */}
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
             </div>
         </div>
     )
