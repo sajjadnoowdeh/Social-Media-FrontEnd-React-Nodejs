efd227f1e105413611d009811c33b27f51d63c97
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-06 19:06:31 +0430

    clean code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Home from "./pages/Home"
 function App() {
     return (
-        <div>
-            sd
+        <>
+            <Home />
             
-        </div>
+        </>
     )
 }
 
