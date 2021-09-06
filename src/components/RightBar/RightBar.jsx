be294994f33114816541d1c7fefb76c9f4a1d787
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-06 19:05:45 +0430

    add right bar

---
+++ src/components/RightBar/RightBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./RightBar.style.css"
+export default function RightBar() {
+    return (
+        <div className="rightbar">
+            Right bar
+        </div>
+    )
+}
