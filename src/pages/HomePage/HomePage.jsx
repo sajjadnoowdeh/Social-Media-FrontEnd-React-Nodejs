7b4f437fcb1d92b723a88c0acc1c0afd650dd769
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-07 22:23:16 +0430

    add home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import TopBar from '../components/TopBar/TopBar'
-import Sidebar from "../components/Sidebar/Sidebar"
-import RightBar from "../components/RightBar/RightBar";
-import Feed from "../components/Feed/Feed"
-import "./home.style.css"
+import TopBar from '../../components/TopBar/TopBar'
+import Sidebar from "../../components/Sidebar/Sidebar"
+import RightBar from "../../components/RightBar/RightBar";
+import Feed from "../../components/Feed/Feed"
+import "./homePage.style.css"
 export default function Home() {
     return (
         <>
