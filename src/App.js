c8e2db7458019639fc8505b9951456f292671413
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-07 22:21:18 +0430

    add pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import Home from "./pages/Home"
+import Home from "./pages/HomePage/HomePage"
+import LoginPage from './pages/LoginPage/LoginPage'
+import Profile from './pages/Profile/Profile'
+import Register from './pages/RegisterPage/RegisterPage'
 function App() {
     return (
         <>
