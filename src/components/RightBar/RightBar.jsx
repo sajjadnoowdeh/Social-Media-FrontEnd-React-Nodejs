bdd46cdc30466e0b45db25554556c54e256b6c80
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-07 22:22:15 +0430

    add profile right bar

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import OnlineUsers from '../OnlineUsers/OnlineUsers'
 import { Users } from '../../dummyData'
 import "./RightBar.style.css"
-export default function RightBar() {
-    return (
-        <div className="rightbar">
-            <div className="rightbarWrapper">
-                <div className="birthdayContainer">
+export default function RightBar({profile}) {
+    const HomeRightBar = ()=>{
+        return(
+            <>
+            <div className="birthdayContainer">
                     <img className="birthdayImg" src="/assets/gift.png" alt="" />
                     <span className="birthdayText"><b>Pola Faster </b>and <b>and 3 friends</b> birthday </span>
                 </div>
@@ -24,6 +24,63 @@ export default function RightBar() {
                        ))
                    }
                 </ul>
+            </>
+        )
+    }
+    const ProfileRightBar =()=>{
+        return(
+            <>
+               <h4 className="rightbarTitle">User Information</h4>
+               <div className="rightbarInfo">
+                     <div className="rightbarInofoItem">
+                         <span className="rightbarInfoKey">city:</span>
+                         <span className="rightbarInfoValue">Mashhad</span>
+                     </div>
+                     <div className="rightbarInofoItem">
+                         <span className="rightbarInfoKey">from:</span>
+                         <span className="rightbarInfoValue">Mashhad</span>
+                     </div>
+                     <div className="rightbarInofoItem">
+                         <span className="rightbarInfoKey">Relationship:</span>
+                         <span className="rightbarInfoValue">Single</span>
+                     </div>
+               </div>
+               <h4 className="rightbarTitle">User Friends</h4>
+               <div className="rightbarFollowings">
+                   <div className="rightbarFollowing">
+                       <img className="rightbarFollowingImg" src="/assets/person/1.jpeg" alt="" />
+                       <span className="rightbarFllowingName">Jane cener</span>
+                   </div>
+                   <div className="rightbarFollowing">
+                       <img className="rightbarFollowingImg" src="/assets/person/2.jpeg" alt="" />
+                       <span className="rightbarFllowingName">Jane cener</span>
+                   </div>
+                   <div className="rightbarFollowing">
+                       <img className="rightbarFollowingImg" src="/assets/person/3.jpeg" alt="" />
+                       <span className="rightbarFllowingName">Jane cener</span>
+                   </div>
+
+                   <div className="rightbarFollowing">
+                       <img className="rightbarFollowingImg" src="/assets/person/4.jpeg" alt="" />
+                       <span className="rightbarFllowingName">Jane cener</span>
+                   </div>
+                   <div className="rightbarFollowing">
+                       <img className="rightbarFollowingImg" src="/assets/person/5.jpeg" alt="" />
+                       <span className="rightbarFllowingName">Jane cener</span>
+                   </div>
+                   <div className="rightbarFollowing">
+                       <img className="rightbarFollowingImg" src="/assets/person/5.jpeg" alt="" />
+                       <span className="rightbarFllowingName">Jane cener</span>
+                   </div>
+                  
+               </div>
+            </>
+        )
+    }
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+               {profile ? <ProfileRightBar /> : <HomeRightBar />} 
             </div>
         </div>
     )
