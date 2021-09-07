2aca7027c7c7519dffdc59875816d19a23dd4d04
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-07 11:25:30 +0430

    update post in map data

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import Share from '../Share/Share';
-import Post from '../PostFeed/PostFeed';
+import Post from '../post/Post';
+import {Posts} from "../../dummyData"
 import "./Feed.style.css"
 export default function Feed() {
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share/>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                {
+                    Posts.map((p)=>(
+                        <Post 
+                        key={p.id} 
+                        post={p}
+                      />
+
+                    ))
+                }
+                
             </div>
         </div>
     )
