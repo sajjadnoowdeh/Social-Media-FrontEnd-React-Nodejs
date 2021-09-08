828760247946a06dc0d9636beebe8ae2b4a3480a
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-09 00:38:10 +0430

    add router

--- src/routers/routerMain.js
+++ src/routers/routerMain.js
@@ -2,9 +2,13 @@
 import React from "react"
 const HomePage = React.lazy(()=>import("../pages/HomePage/HomePage"))
 const Profile = React.lazy(()=>import("../pages/Profile/Profile"))
+const LoginPage = React.lazy(()=>import('../pages/LoginPage/LoginPage'))
+const Register =  React.lazy(()=>import('../pages/RegisterPage/RegisterPage'))
 
 export let routers = [
     {path:"/",exact:true, Component:HomePage},
-    {path:"/profile",exact:true, Component:Profile}
+    {path:"/profile",exact:true, Component:Profile},
+     {path:"/profile",exact:true, Component:Profile},
+      {path:"/profile",exact:true, Component:Profile}
 ]
 
