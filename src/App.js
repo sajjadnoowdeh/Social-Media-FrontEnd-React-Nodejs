9f352530466780bc4b1a14fec6de207c02dc2884
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-09 00:39:10 +0430

    add lazy load

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import Home from "./pages/HomePage/HomePage"
-import LoginPage from './pages/LoginPage/LoginPage'
-import Profile from './pages/Profile/Profile'
-import Register from './pages/RegisterPage/RegisterPage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+const HomePage = React.lazy(()=>import("./pages/HomePage/HomePage"))
+const LoginPage = React.lazy(()=>import('./pages/LoginPage/LoginPage'))
+const Profile = React.lazy(()=>import('./pages/Profile/Profile'))
+const Register = React.lazy(()=>import('./pages/RegisterPage/RegisterPage'))
+
 function App() {
     return (
+        
         <>
-            <Home />
+            <Router>
+                <Switch>
+                    <Route exact={true} path="/" >
+                        <HomePage />
+                    </Route>
+                    <Route exact={true} path="/login">
+                        <LoginPage/>
+                    </Route>
+                    <Route exact={true} path="/register">
+                        <Register/>
+                    </Route>
+                    <Route exact={true} path="/profile/:username">
+                       <Profile/>
+                    </Route>
+                </Switch>
+            </Router>
             
         </>
     )
