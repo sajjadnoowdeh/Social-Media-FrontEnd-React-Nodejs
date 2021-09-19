8ea370afe8e04d91c7f280bac6889d379c745d7a
sajjad.noowdeh <noodesajjad18@gmail.com>
2021-09-19 15:44:06 +0430

    config private route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+
+import { ContextAuth } from './context/AuthContext';
+import { Redirect } from 'react-router';
 const HomePage = React.lazy(()=>import("./pages/HomePage/HomePage"))
 const LoginPage = React.lazy(()=>import('./pages/LoginPage/LoginPage'))
 const Profile = React.lazy(()=>import('./pages/Profile/Profile'))
 const Register = React.lazy(()=>import('./pages/RegisterPage/RegisterPage'))
-
 function App() {
+    const {user} = useContext(ContextAuth)
     return (
         
         <>
             <Router>
                 <Switch>
                     <Route exact={true} path="/" >
+                       {user ? <HomePage />: <Redirect to="/register" />}
                         <HomePage />
                     </Route>
                     <Route exact={true} path="/login">
-                        <LoginPage/>
+                        {user ? <Redirect to="/"/> : <LoginPage/>}
+                        
                     </Route>
                     <Route exact={true} path="/register">
-                        <Register/>
+                        {user ? <Redirect to="/"/> :     <Register/>}
+                    
                     </Route>
                     <Route exact={true} path="/profile/:username">
                        <Profile/>
